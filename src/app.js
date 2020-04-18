e67f2d6f7ca25ce552c88dee4627b6807a590a66
namhihi237 <yentth237@gmail.com>
2020-04-18 14:17:23 +0700

    add config express

--- src/app.js
+++ src/app.js
@@ -1,77 +1,48 @@
 require('dotenv-safe').config({
     example: process.env.CI ? '.env.ci.example' : '.env.example',
 });
-const express = require('express');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
-const path = require('path');
+
 const moongose = require('./config/mongoose');
-
-const watherRouter = require('./routes/weather.route');
-const homeRouter = require('./routes/home.route');
-const tempChartRoute = require('./routes/temp.chart.route');
-const humidityChartRoute = require('./routes/humidity.chart.router')
-const pressureChartRoute = require('./routes/pressure.chart.route')
-
-
-const app = express();
+const app = require('../src/config/express');
 // import socket io
+
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const PORT = process.env.PORT;
 
 
-
-app.set('views', './src/views');
-app.set('view engine', 'pug');
-app.use(express.static('./src/public'));
-app.use(express.static(path.join(__dirname, '/')));
-app.use(bodyParser.json());
-app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    }),
-);
-app.use(logger('dev'));
-
 moongose.connect(); // connect database
 server.listen(PORT, () => console.log(`app run port = ${PORT}`));
 
-app.use('/predict', watherRouter);
-app.use('/home', homeRouter);
-app.use('/temperature', tempChartRoute)
-app.use('/humidity', humidityChartRoute)
-app.use('/pressure', pressureChartRoute)
-
 // socketio
 io.on('connection', (socket) => {
-    socket.join("room-x");
+    socket.join('room-x');
     console.log(socket.id + ' connected');
     socket.on('disconnect', () => {
         console.log(socket.id + ' disconnect');
     });
     // send data to Temperature chart
-    socket.on("Client-send-temp", async(mess) => {
+    socket.on('Client-send-temp', async(mess) => {
         // After 1 second the server sends the last 20 records
         setInterval(async() => {
             let data = await require('./controllers/temp.chart.controller').getData();
-            socket.emit("Sv-send", data)
-        }, 1000)
-    })
-
-    socket.on("Client-send-humidity", async(mess) => {
+            socket.emit('Sv-send', data);
+        }, 1000);
+    });
+    // send data to humidity chart
+    socket.on('Client-send-humidity', async(mess) => {
         // After 1 second the server sends the last 20 records
         setInterval(async() => {
             let data = await require('./controllers/temp.chart.controller').getData();
-            socket.emit("Sv-send", data)
-        }, 1000)
-    })
-
-    socket.on("Client-send-pressure", async(mess) => {
+            socket.emit('Sv-send', data);
+        }, 1000);
+    });
+    // send data to pressure chart
+    socket.on('Client-send-pressure', async(mess) => {
         // After 1 second the server sends the last 20 records
         setInterval(async() => {
             let data = await require('./controllers/temp.chart.controller').getData();
-            socket.emit("Sv-send", data)
-        }, 1000)
-    })
+            socket.emit('Sv-send', data);
+        }, 1000);
+    });
 });
---
+++ src/config/express.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const path = require('path');
+
+const watherRouter = require('../routes/weather.route');
+const homeRouter = require('../routes/home.route');
+const tempChartRoute = require('../routes/temp.chart.route');
+const humidityChartRoute = require('../routes/humidity.chart.router');
+const pressureChartRoute = require('../routes/pressure.chart.route');
+const app = express();
+
+app.set('views', './src/views');
+app.set('view engine', 'pug');
+app.use(express.static('./src/public'));
+app.use(express.static(path.join(__dirname, '/')));
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    }),
+);
+app.use(logger('dev'));
+
+app.use('/predict', watherRouter);
+app.use('/', homeRouter);
+app.use('/temperature', tempChartRoute);
+app.use('/humidity', humidityChartRoute);
+app.use('/pressure', pressureChartRoute);
+
+module.exports = app
