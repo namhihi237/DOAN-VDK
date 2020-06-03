20318ac6b876b7f840a252f615470e2d6b77517f
namhihi237 <yentth237@gmail.com>
2020-06-03 12:47:08 +0700

    a

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -5,7 +5,7 @@ require("dotenv-safe").config({
 // config connect mongodb
 exports.connect = async() => {
     try {
-        await mongoose.connect(process.env.MONGO_URL, {
+        await mongoose.connect(process.env.MONGO, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
             useCreateIndex: true,
